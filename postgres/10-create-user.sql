d813830ed9ced883c34bd9ff8f9a1a77a8ec21e1
David-Martinez1 <2445882602@qq.com>
2024-12-04 04:19:07 +0800

    Update 10-create-user.sql

--- postgres/10-create-user.sql
+++ postgres/10-create-user.sql
@@ -24,7 +24,7 @@ CREATE TABLE hotels
 );
 INSERT INTO hotels (id, hotel_uid, name, country, city, address, stars, price) VALUES (1, '049161bb-badd-4fa8-9d90-87c9a82b0668', 'Ararat Park Hyatt Moscow', 'Россия', 'Москва', 'Неглинная ул., 4', 5, 10000);
 CREATE TABLE reservation
-(
+( 
     id              SERIAL PRIMARY KEY,
     reservation_uid uuid UNIQUE NOT NULL,
     username        VARCHAR(80) NOT NULL,
